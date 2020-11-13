96319f6581f74eb0c4ffac18d76a094746715a30
shane <shanecolbyzero8@gmail.com>
2020-11-12 19:34:50 -0600

    added latitude/longitude columns to model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_13_005305) do
+ActiveRecord::Schema.define(version: 2020_11_13_013229) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,8 @@ ActiveRecord::Schema.define(version: 2020_11_13_005305) do
     t.decimal "price"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.float "latitude"
+    t.float "longitude"
   end
 
 end
